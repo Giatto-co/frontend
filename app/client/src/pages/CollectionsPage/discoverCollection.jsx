11ed2612c96ad58272f00f6c9051dc88be01f43d
Sarah Adebesin <sarahjoseph@ymail.com>
2023-04-12 18:01:55 +0100

    removed unused code

--- app/client/src/pages/CollectionsPage/discoverCollection.jsx
+++ app/client/src/pages/CollectionsPage/discoverCollection.jsx
@@ -6,7 +6,6 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 const itemData = [
   {
